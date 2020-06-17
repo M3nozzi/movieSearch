148fa32c19f2062d3a2d073067767d47d7e80baa
Fabio Menozzi <f.menozzi@yahoo.com>
2020-06-17 16:45:54 -0300

    removing files that are not necessary

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -4,5 +4,5 @@ import App from '../main/App';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
-});
+    ReactDOM.render(<App />, div);
+});d
