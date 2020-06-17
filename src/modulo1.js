4e65116fc23c295c8875ab9c83fb991626ef18ba
Fabio Menozzi <f.menozzi@yahoo.com>
2020-06-17 03:24:02 -0300

    removed files and folder not necessary

--- src/modulo1.js
+++
@@ -1,5 +0,0 @@
-const modulo1 = {
-    func1: (a) => a+1
-}
-
-module.exports = modulo1
--- src/modulo1.test.js
+++
@@ -1,7 +0,0 @@
-const modulo1 = require('./modulo1.js')
-
-describe('basic feature', () => {
-    it('should return the right value', () => {
-        expect(modulo1.func1(10)).toBe(11)
-    })
-} )
